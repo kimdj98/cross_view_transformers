d825c97244b3ce503a53134297ddd9975d94f832
kdj <kimdj98g@gmail.com>
2023-08-15 00:55:18 +0900

    change scheduler to LambdaLR

--- cross_view_transformer/model/model_module.py
+++ cross_view_transformer/model/model_module.py
@@ -82,12 +82,12 @@ class ModelModule(pl.LightningModule):
             v.sampler.shuffle = True
             # v.sampler.set_epoch(self.current_epoch)
 
-    def configure_optimizers(self, disable_scheduler=False):
+    def configure_optimizers(self, disable_scheduler=True):
         parameters = [x for x in self.backbone.parameters() if x.requires_grad]
         optimizer = torch.optim.AdamW(parameters, **self.optimizer_args)
 
         if disable_scheduler or self.scheduler_args is None:
-            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda lr: 1)
+            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda step: 0.99997**step)
         else:
             scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, **self.scheduler_args)
 
